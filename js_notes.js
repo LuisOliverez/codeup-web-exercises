492764266f85766ab4246468b95cdb3b80e23fba
Luis Oliverez <louoliverez901@gmail.com>
2023-03-13 10:20:54 -0700

    Updating Notes

--- js_notes.js
+++ js_notes.js
@@ -1,7 +1,8 @@
 //NOTES
 //-------
 //Argument is the value that a function is called with, and a parameter is part of the function's definition. A function is defined with zero or more parameters and called with zero or more arguments.
-
+'use strict';
+(function)
 
 //SCOPE is a term that describes where a variable can be accessed. If a variable is in-scope, that means that it can be accessed from that location in the code. Conversely, if a variable is out-of-scope, that means it is not accessible.
 
@@ -19,17 +20,62 @@
 //    var result = num1 * num2 * num3;
 //    alert("My result is: " + result)
   //  return result;
-}
+//}
 //multiply(num1: 12, num2: 3, num3: 17);
 //console.log(multiply())
 
 
-function increment(x) {
-    return x + 1;
+//  function increment(x) {
+//    return x + 1;
+//  }
+//  var four = increment(x)3);
+//  var six = increment(increment(x:3)));
+
+//  console.log(four)
+//  console.log(six)
+
+//if console.log after RETURN statement, will not console.log
+// (timeing)
+
+
+//ANONYMOUS FUNCTION
+
+//var increment = function(x) {
+//    return x + 1;
+//   };
+//ANONYMOUS part is function(x) {
+//                  return x + 1;
+// var increment = stored the function to use later
+
+//  var two = increment(1);
+//  var numbers = (2,4,6,8)
+
+//  numbers.forEach(Function(num){
+//    console.log(num += 5)
+//  }
+
+//
+// function sayHello(userName){
+//     alert("welcome, " + userName);
+// }
+//
+// var nameInput = prompt("what is your name?");
+// sayHello(nameInput);
+// console.log()
+
+//Declare a global variable
+// var globalVar = "Look, I'm Global!";
+//
+// //define the sayHello function
+// function sayHello(){
+//     //globalVar is accessible hre since the sayHello function scope is a sub-scope of the global scope
+//     alert(globalVar);
+// }
+// sayHello();
+
+
+function favoreiteJunkFood(){
+    var pizza = "Round Table";
+    var fries = "In N Out";
+    var
 }
-var four = increment(x:3);
-var six = increment(increment(x:3)));
-
-console.log(four)
-
-//if console.log after RETURN statement, will not console.log(timeing)
