875070694b6dbb08d74b9ab1e7be7a6e932f90b2
Luis Oliverez <louoliverez901@gmail.com>
2023-03-20 14:04:36 -0700

    begining objects

---
+++ js/objects.js
@@ -0,0 +1,13 @@
+
+
+
+
+var waterBottle = {
+    size: "20oz",
+    color: 'Blue',
+    brand: 'CamleBack',
+}
+
+console.log(waterBottle)
+
+console.log("The color of  my waterbottle is: " + waterBottle.color);
