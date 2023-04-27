f777468a275b92cd23e2de949506e487d2337356
Luis Oliverez <louoliverez901@gmail.com>
2023-04-27 16:08:20 -0700

    adding ignore and keys again

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-const MAPBOX_KEY = 'pk.eyJ1IjoibG9saXZlcmV6IiwiYSI6ImNsZ3kyZ3ZjdDAybTYzc3Nha29scGlmZjQifQ.M0q3_1Nzj6StCDRfh8NA9g';
-
-const OPEN_WEATHER_KEY = 'b6bfed76c0b43ccc94a2934a1a6827d0';
-
-const MYRADAR_KEY = 'https://api.myradar.dev/forecast/{latitude},{longitude}[?extend][&units][&lang][&exclude]'
