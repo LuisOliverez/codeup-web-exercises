c98672f99cc8a315b16f4d629e5d550a7eb3608c
Luis Oliverez <louoliverez901@gmail.com>
2023-04-29 15:52:37 -0700

    sorting file structure

---
+++ js/weather_map.js
@@ -0,0 +1,29 @@
+
+
+
+
+mapboxgl.accessToken = MAPBOX_KEY;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+});
+
+$.get("https://api.openweathermap.org/data/3.0/onecall?appid={MAPBOX_KEY}", {
+    APPID: OPEN_WEATHER_KEY,
+    q:     "Pasco, US",
+    units: 'imperial',
+    current: 'current.temp'
+
+
+}).done(function (data){
+    $("#temperature").text(data.main.temp);
+    // $('#temperature').text(data.main.clouds);
+    console.log(data);
+}).fail(function (jqXHR, testStatus, errorThrow){
+    console.error(errorThrow);
+
+});
+
+
