e17aff6c0f3ae1fc9159ca43d8240f84032291cf
Luis Oliverez <louoliverez901@gmail.com>
2023-05-01 13:21:04 -0700

    added geolocator code for initilizing map, commented out for responsivness while bulding

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,28 +1,46 @@
+"use strict";
+
 import { MAPBOX_KEY, OPEN_WEATHER_KEY} from './keys.js';
 
 
+
+// GEO-LOCATOR MAP
+
+// mapboxgl.accessToken = MAPBOX_KEY;
+//
+//     const map = new mapboxgl.Map({
+//         container: 'map', // container ID
+//         style: 'mapbox://styles/mapbox/outdoors-v12', // style URL/STYLE OF MAP
+//     });
+//
+//     //GEOLOCATOR FUNCTION
+// navigator.geolocation.getCurrentPosition (function(position) {
+//     const lat = position.coords.latitude;
+//     const lon = position.coords.longitude;
+//     map.setCenter([lon, lat]);//SET CENTER TO GEOLOCATION LAT/LON
+// });
+// map.setZoom(9); //SET ZOOM
+
+
 mapboxgl.accessToken = MAPBOX_KEY;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-119.09207, 46.22951], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-});
 
 
-// GEO-LOCATOR
-// navigator.geolocation.getCurrentPosition(function (position){
-//  const lat = position.coords.latitude;
-//  const lon = position.coords.longitude;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
+        center: [-119.09207, 46.22951], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+    });
 
- // API CALL
+
+
+
+ // CURRENT WEATHER API CALL
 $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${46.22951}&lon=${-119.09207}&appid=${OPEN_WEATHER_KEY}&units=imperial`)
     .done(function (data){
-        $('#temperature').text(data.main.temp+'\u00b0 F'),
-        $('#location').text(data.name),
-        $('#sky-condition').text(data.weather[0].description),
+        $('#temperature').text('Current Weather in: '+data.name+' '+data.main.temp+'\u00b0 F'),
+        $('#cloud-condition').text('Cloud Condition: '+ data.weather[0].description),
         $('#feels-like').text('Feels Like: '+data.main.feels_like+'\u00b0 F'),
-        $('#condition').text(data.main.contition),
         $('#max-temp').text('High For The Day: '+data.main.temp_max+'\u00b0 F'),
         $('#min-temp').text('Low For The Day: '+data.main.temp_min+'\u00b0 F')
         console.log(data);
@@ -33,9 +51,9 @@ $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${46.22951}&lon=${-11
     });
 
 
-
-
-
-
-
+// FORCAST API CALL
+$.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${46.22951}&lon=${-119.09207}&appid=${OPEN_WEATHER_KEY}&units=imperial`)
+    .done(function (data){
+        console.log(data)
+    })
 
