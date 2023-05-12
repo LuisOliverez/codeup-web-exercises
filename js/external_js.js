30535f450166a240e2b50c39ee16aac2ef682505
Luis Oliverez <louoliverez901@gmail.com>
2023-05-12 10:19:28 -0700

    changed file name

--- js/external_js.js
+++ js/external_js.js
@@ -1,56 +1,56 @@
 "use strict"
 
-//console.log ("Hello from external JavaScript.");
+console.log ("Hello from external JavaScript.");
 
-//var userColor = prompt("What is your favorite color?");
+var userColor = prompt("What is your favorite color?");
 
-//alert("Great, " + userColor + " is my favorite color too!");
-//console.log(userColor)
+alert("Great, " + userColor + " is my favorite color too!");
+console.log(userColor)
 
 //JS with HTML Exc.
 
 //Exc 3.1
-//var littleMermaidDays = + (prompt("How many was Little Mermaid rented?"));
-//var brotherBearDays = + (prompt("How many days was Brother Bear rented?"));
-//var herculesDays = + (prompt("How many days was Hercules rented?"));
-//var rentalPrice = 3;
-//var totalCost = (littleMermaidDays + brotherBearDays + herculesDays) * rentalPrice;
-//alert("The total rental cost is $" + totalCost.toFixed(2));
-//console.log("$" + totalCost)
+var littleMermaidDays = + (prompt("How many was Little Mermaid rented?"));
+var brotherBearDays = + (prompt("How many days was Brother Bear rented?"));
+var herculesDays = + (prompt("How many days was Hercules rented?"));
+var rentalPrice = 3;
+var totalCost = (littleMermaidDays + brotherBearDays + herculesDays) * rentalPrice;
+alert("The total rental cost is $" + totalCost.toFixed(2));
+console.log("$" + totalCost)
 
 //Exc 3.2
 
-//var googleRate = 400;
-//var amazonRate = 380;
-//var faceBookRate = 350;
+var googleRate = 400;
+var amazonRate = 380;
+var faceBookRate = 350;
 
-//var googleHours = prompt("How many hours did you work for Google?");
-//var amazonHours = prompt("How many hours did you work for Amazon?");
-//var faceBooKHours = prompt("How many hours did you work for FaceBook?");
+var googleHours = prompt("How many hours did you work for Google?");
+var amazonHours = prompt("How many hours did you work for Amazon?");
+var faceBooKHours = prompt("How many hours did you work for FaceBook?");
 
-//var googleHoursNum = Number(googleHours);
-//var amazonHoursNum = Number(amazonHours);
-//var facebookNum = Number(faceBooKHours);
+var googleHoursNum = Number(googleHours);
+var amazonHoursNum = Number(amazonHours);
+var facebookNum = Number(faceBooKHours);
 
-//var totalPayment = (googleHoursNum * googleRate) + (amazonHoursNum * amazonRate)
-//+ (facebookNum * faceBookRate);
+var totalPayment = (googleHoursNum * googleRate) + (amazonHoursNum * amazonRate)
++ (facebookNum * faceBookRate);
 
-//alert('Total payment:$' + totalPayment);
-//console.log("$" + totalPayment)
+alert('Total payment:$' + totalPayment);
+console.log("$" + totalPayment)
 
 
 //Exc 3.3
 
-//var isClassFull = prompt("Is the class full? (Answer 'yes' or 'no')");
-//var scheduleConflict = prompt("Does your current schedule conflict? (Answer 'yes' or 'no')");
-//var canEnroll = (isClassFull.toLocaleLowerCase() !== "yes")&&(scheduleConflict.toLocaleLowerCase() !== "yes");
+var isClassFull = prompt("Is the class full? (Answer 'yes' or 'no')");
+var scheduleConflict = prompt("Does your current schedule conflict? (Answer 'yes' or 'no')");
+var canEnroll = (isClassFull.toLocaleLowerCase() !== "yes")&&(scheduleConflict.toLocaleLowerCase() !== "yes");
 
-// if (canEnroll) {
-//    alert('You can enroll in this class!');
-// } else {
-//    alert('You cannot enroll in the class.');
-// }
-// console.log(canEnroll)
+if (canEnroll) {
+   alert('You can enroll in this class!');
+} else {
+   alert('You cannot enroll in the class.');
+}
+console.log(canEnroll)
 
 
 //Exc 3.4
